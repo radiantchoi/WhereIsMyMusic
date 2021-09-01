191808fb7b97cea087deef3925151c79fd4ca37b
Gordon Choi <thefirate@gmail.com>
2021-09-02 01:09:03 +0900

    Initial setup

---
+++ WhereIsMyMusic/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewController.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2021/09/02.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

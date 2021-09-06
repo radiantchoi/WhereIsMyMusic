cb933cda6dff8ebd902ca0bb3772de50004bf81a
Gordon Choi <thefirate@gmail.com>
2021-09-07 01:39:22 +0900

    Applying ShazamKit with solution provided by iOSAcademy (should be debugged)

--- WhereIsMyMusic/SongResultViewController.swift
+++ WhereIsMyMusic/SongResultViewController.swift
@@ -8,29 +8,82 @@
 import UIKit
 import ShazamKit
 
-@available(iOS 15.0, *)
-class SongResultViewController: UIViewController {
 
+@available(iOS 15.0, *)
+class SongResultViewController: UIViewController, SHSessionDelegate {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var shazamButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
-    let session = SHSession()
-    session.delegate = self
     
-    let signatureGenerator = SHSignatureGenerator()
-    try signatureGenerator.append(buffer, at: nil)
+    //MARK: Shazam song recognizing logic (reference by iOSAcademy)
+    private func recognizeSong() {
+        // Session
+        let session = SHSession()
+        
+        // Delegate
+        session.delegate = self
+        
+        do {
+            // Get track
+            guard let url = Bundle.main.url(forResource: "song", withExtension: "mp3") else {
+                print("Failed to get song url")
+                return
+            }
+            
+            // Create Audio File
+            let file = try AVAudioFile(forReading: url)
+            
+            // Audio -> Buffer
+            guard let buffer = AVAudioPCMBuffer(
+                pcmFormat: file.processingFormat,
+                frameCapacity: AVAudioFrameCount(file.length / 30)
+            ) else {
+                print("Failed to create buffer")
+                return
+            }
+            try file.read(into: buffer)
+            
+            // SignatureGenerator
+            let generator = SHSignatureGenerator()
+            try generator.append(buffer, at: nil)
+            
+            // Create Signature
+            let signature = generator.signature()
+            
+            // Try to match
+            session.match(signature)
+            
+        }
+        catch {
+            print(error)
+        }
+    }
     
-    let signature = signatureGenerator.signature()
-    session.match(signature)
-}
-
-@available(iOS 15.0, *)
-extension SongResultViewController: SHSessionDelegate {
+    func session(_ session: SHSession, didFind match: SHMatch) {
+        let items = match.mediaItems
+        items.forEach { item in
+            print(item.title ?? "title")
+            print(item.artist ?? "artist")
+            print(item.artworkURL?.absoluteURL ?? "Artwork url")
+//            titleLabel.text = item.title ?? "title"
+//            artistLabel.text = item.artist ?? "artist"
+        }
+    }
     
-    public func session(_ session: SHSession, didFind match: SHMatch) {
-        guard let matchedMediaItem = match.mediaItems.first else { return }
-        print(matchedMediaItem)
+    func session(_ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?) {
+        if let error = error {
+            print(error)
+        }
+    }
+    
+    
+    @IBAction private func buttonPressed(_ sender: UIButton) {
+        recognizeSong()
     }
 }

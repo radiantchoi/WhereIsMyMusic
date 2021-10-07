027babc02958804de11272267655e21f0bb5dd6f
Gordon Choi <thefirate@gmail.com>
2021-10-07 21:51:13 +0900

    Refactor search view controller and create result view controller

---
+++ WhereIsMyMusic/SearchResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResultViewController.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2021/10/07.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- WhereIsMyMusic/ShazamSearchViewController.swift
+++ WhereIsMyMusic/ShazamSearchViewController.swift
@@ -1,6 +1,6 @@
 //
-//  SongResultViewController.swift
-//  SongResultViewController
+//  ShazamSearchViewController.swift
+//  ShazamSearchViewController
 //
 //  Created by Gordon Choi on 2021/09/02.
 //
@@ -9,20 +9,15 @@ import UIKit
 import ShazamKit
 import AVFoundation
 
-class SongResultViewController: UIViewController {
+class ShazamSearchViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var artistLabel: UILabel!
-    @IBOutlet weak var albumLabel: UILabel!
     @IBOutlet weak var shazamButton: UIButton!
-    @IBOutlet weak var checkLabel: UILabel!
     
     var mediaItem: SHMediaItem?
     var error: Error?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkLabel.isHidden = true
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(updateUI),
                                                name: ShazamController.matchFoundNotification,
@@ -30,7 +25,7 @@ class SongResultViewController: UIViewController {
     }
 }
 
-extension SongResultViewController {
+extension ShazamSearchViewController {
     
     @objc func updateUI() {
         self.mediaItem = ShazamController.shared.mediaItem ?? nil
@@ -38,13 +33,9 @@ extension SongResultViewController {
         DispatchQueue.main.async {
             if self.mediaItem == nil {
                 print(self.error as Any)
-                self.titleLabel.text = "Not found"
-                self.artistLabel.text = "Not found"
-                self.albumLabel.text = "Not found"
+                
             } else {
-                self.titleLabel.text = self.mediaItem?.title
-                self.artistLabel.text = self.mediaItem?.artist
-                self.albumLabel.text = self.mediaItem?.album
+                
             }
         }
     }
@@ -52,12 +43,9 @@ extension SongResultViewController {
 }
 
 
-extension SongResultViewController {
+extension ShazamSearchViewController {
 
     @IBAction private func buttonPressed(_ sender: UIButton) {
-        titleLabel.text = "Searching..."
-        artistLabel.text = "Searching..."
-        albumLabel.text = "Searching..."
         ShazamController.shared.start()
     }
 

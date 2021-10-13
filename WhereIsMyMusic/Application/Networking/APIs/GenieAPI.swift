cedfbb74ed0b8a269194b5bd88506d932f8e3474
Gordon Choi <thefirate@gmail.com>
2021-10-14 02:05:23 +0900

    Modify crawl manager and apis to get 3 results

--- WhereIsMyMusic/Application/Networking/APIs/GenieAPI.swift
+++ WhereIsMyMusic/Application/Networking/APIs/GenieAPI.swift
@@ -10,14 +10,20 @@ import Foundation
 struct GenieAPI {
     let baseURL = URL(string: "https://www.genie.co.kr/search/searchSong")!
     let cssQuery = "#body-content > div.search_song > div.music-list-wrap > div.music-list-wrap > table > tbody"
-    let titleCss = "tr:nth-child(1) > td.info > a.title.ellipsis"
-    let artistCss = "tr:nth-child(1) > td.info > a.artist.ellipsis"
-    let albumCss = "tr:nth-child(1) > td.info > a.albumtitle.ellipsis"
+    let titleCss = ["tr:nth-child(1) > td.info > a.title.ellipsis",
+                    "tr:nth-child(2) > td.info > a.title.ellipsis",
+                    "tr:nth-child(3) > td.info > a.title.ellipsis"]
+    let artistCss = ["tr:nth-child(1) > td.info > a.artist.ellipsis",
+                     "tr:nth-child(2) > td.info > a.artist.ellipsis",
+                     "tr:nth-child(3) > td.info > a.artist.ellipsis"]
+    let albumCss = ["tr:nth-child(1) > td.info > a.albumtitle.ellipsis",
+                    "tr:nth-child(2) > td.info > a.albumtitle.ellipsis",
+                    "tr:nth-child(3) > td.info > a.albumtitle.ellipsis"]
     var query: Query = [:]
 }
 
 extension GenieAPI {
-    func loadGenieSong(completion: @escaping (GenieSong?) -> Void) {
+    func loadGenieSong(completion: @escaping ([GenieSong]?) -> Void) {
 
         CrawlManager.shared.crawl(EndPoint(baseURL: baseURL,
                                            query: query,
@@ -27,18 +33,22 @@ extension GenieAPI {
                                   artistCss: artistCss,
                                   albumCss: albumCss) {
             switch $0 {
-            case .success(let data):
-                var titleData: String = ""
-                
-                let titleSpread = data[0].map { String($0) }
-                if titleSpread.count > 6 && titleSpread[0...5].joined() == "TITLE " {
-                    titleData = titleSpread[6...].joined()
-                } else {
-                    titleData = titleSpread.joined()
+            case .success(let datas):
+                var genieSongs = [GenieSong]()
+                for data in datas {
+                    var titleData: String = ""
+                    let titleSpread = data[0].map { String($0) }
+                    if titleSpread.count > 6 && titleSpread[0...5].joined() == "TITLE " {
+                        titleData = titleSpread[6...].joined()
+                    } else {
+                        titleData = titleSpread.joined()
+                    }
+                    
+                    let genieSong = GenieSong.init(title: titleData, artist: data[1], album: data[2], imageURL: nil)
+                    genieSongs.append(genieSong)
                 }
+                completion(genieSongs)
                 
-                let genieSong = GenieSong.init(title: titleData, artist: data[1], album: data[2], imageURL: nil)
-                completion(genieSong)
             case .failure(_):
                 completion(nil)
             }
--- WhereIsMyMusic/Application/Networking/APIs/MelonAPI.swift
+++ WhereIsMyMusic/Application/Networking/APIs/MelonAPI.swift
@@ -10,14 +10,20 @@ import Foundation
 struct MelonAPI {
     let baseURL = URL(string: "https://www.melon.com/search/song/index.htm")!
     let cssQuery = "#frm_defaultList > div > table > tbody"
-    let titleCss = "tr:nth-child(1) > td:nth-child(3) > div > div > a.fc_gray"
-    let artistCss = "#artistName > a"
-    let albumCss = "tr:nth-child(1) > td:nth-child(5) > div > div > a"
+    let titleCss = ["tr:nth-child(1) > td:nth-child(3) > div > div > a.fc_gray",
+                    "tr:nth-child(2) > td:nth-child(3) > div > div > a.fc_gray",
+                    "tr:nth-child(3) > td:nth-child(3) > div > div > a.fc_gray"]
+    let artistCss = ["tr:nth-child(1) > td:nth-child(4) > div > div > a",
+                     "tr:nth-child(2) > td:nth-child(4) > div > div > a",
+                     "tr:nth-child(3) > td:nth-child(4) > div > div > a"]
+    let albumCss = ["tr:nth-child(1) > td:nth-child(5) > div > div > a",
+                    "tr:nth-child(2) > td:nth-child(5) > div > div > a",
+                    "tr:nth-child(3) > td:nth-child(5) > div > div > a"]
     var query: Query = [:]
 }
 
 extension MelonAPI {
-    func loadMelonSong(completion: @escaping (MelonSong?) -> Void) {
+    func loadMelonSong(completion: @escaping ([MelonSong]?) -> Void) {
 
         CrawlManager.shared.crawl(EndPoint(baseURL: baseURL,
                                            query: query,
@@ -27,9 +33,13 @@ extension MelonAPI {
                                   artistCss: artistCss,
                                   albumCss: albumCss) {
             switch $0 {
-            case .success(let data):
-                let melonSong = MelonSong.init(title: data[0], artist: data[1], album: data[2], imageURL: nil)
-                completion(melonSong)
+            case .success(let datas):
+                var melonSongs = [MelonSong]()
+                for data in datas {
+                    let melonSong = MelonSong.init(title: data[0], artist: data[1], album: data[2], imageURL: nil)
+                    melonSongs.append(melonSong)
+                }
+                completion(melonSongs)
             case .failure(_):
                 completion(nil)
             }

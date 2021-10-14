a0c771789913aeb1dee0caae232c50d1896da799
Gordon Choi <thefirate@gmail.com>
2021-10-14 19:59:22 +0900

    Set apple api and connection test

---
+++ WhereIsMyMusic/Application/Networking/APIs/AppleAPI.swift
@@ -0,0 +1,27 @@
+//
+//  AppleAPI.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2021/10/14.
+//
+
+import Foundation
+
+struct AppleAPI {
+    let baseURL = URL(string: "https://itunes.apple.com/search")!
+    var query: Query = [:]
+}
+
+extension AppleAPI {
+    func loadAppleSong(completion: @escaping (AppleSongs?) -> Void) {
+        let endPoint = EndPoint(baseURL: baseURL, query: query, httpMethod: .get)
+        NetworkManager.shared.call(endPoint, for: AppleSongs.self) {
+            switch $0 {
+            case .success(let appleSongs):
+                print(appleSongs)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}

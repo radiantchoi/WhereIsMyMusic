9aacf6f311261660ee7d6ca361eb6894cbfd5333
Gordon Choi <thefirate@gmail.com>
2021-10-21 00:06:04 +0900

    Set vibe song model

---
+++ WhereIsMyMusic/Application/Models/VibeSong.swift
@@ -0,0 +1,21 @@
+//
+//  VibeSong.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2021/10/21.
+//
+
+import Foundation
+
+struct VibeSong {
+    let title: String
+    let artist: String
+    let album: String
+    
+    init(vibeSongModel: VibeSongModel) {
+        title = vibeSongModel.trackTitle
+        album = vibeSongModel.albumTitle
+        artist = vibeSongModel.artistName
+    }
+}
+

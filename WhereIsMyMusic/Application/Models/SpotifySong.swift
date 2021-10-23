a08af1c0e561cabe6b5b326ea13757bdf7da2b58
Gordon Choi <thefirate@gmail.com>
2021-10-23 17:32:52 +0900

    Set spotify model

---
+++ WhereIsMyMusic/Application/Models/SpotifySong.swift
@@ -0,0 +1,15 @@
+//
+//  SpotifySong.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2021/10/23.
+//
+
+import Foundation
+
+struct SpotifySong {
+    let title: String
+    let artist: String
+    let album: String
+    
+}

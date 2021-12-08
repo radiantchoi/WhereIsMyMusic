19980e3cb96ccfe0b5239fef9cf377a26661aa37
Gordon Choi <thefirate@gmail.com>
2021-12-08 17:56:41 +0900

    Remove unnecessary functions on table view cells

--- WhereIsMyMusic/Application/Views/TableViewCells/SearchResultTableViewCell.swift
+++ WhereIsMyMusic/Application/Views/TableViewCells/SearchResultTableViewCell.swift
@@ -20,18 +20,3 @@ class SearchResultTableViewCell: UITableViewCell {
     }
     
 }
-
-extension SearchResultTableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-}
--- WhereIsMyMusic/Application/Views/TableViewCells/ShazamResultTableViewCell.swift
+++ WhereIsMyMusic/Application/Views/TableViewCells/ShazamResultTableViewCell.swift
@@ -25,18 +25,3 @@ class ShazamResultTableViewCell: UITableViewCell {
         albumImageView.layer.masksToBounds = true
     }
 }
-
-extension ShazamResultTableViewCell {
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-}

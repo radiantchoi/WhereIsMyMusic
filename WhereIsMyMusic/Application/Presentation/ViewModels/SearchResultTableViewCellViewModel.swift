4eb97459005fca8174fac832d9ca639a74e87227
Gordon Choi <thefirate@gmail.com>
2022-01-13 01:40:19 +0900

    Setup mockups for table view cells view model

---
+++ WhereIsMyMusic/Application/Presentation/ViewModels/SearchResultTableViewCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultTableViewCellViewModel.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2022/01/13.
+//
+
+import Foundation
+
+final class SearchResultTableViewCellViewModel {
+    
+}
---
+++ WhereIsMyMusic/Application/Presentation/ViewModels/ShazamResultTableViewCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ShazamResultTableViewCellViewModel.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2022/01/13.
+//
+
+import Foundation
+
+final class ShazamResultTableViewCellViewModel {
+    
+}

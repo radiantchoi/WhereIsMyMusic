c343697969dbcf617c000ace283627cb7bc90fdf
Gordon Choi <thefirate@gmail.com>
2022-01-22 18:01:05 +0900

    Bind view model into shazam search view controller

--- WhereIsMyMusic/Application/Presentation/ViewModels/ShazamSearchViewViewModel.swift
+++ WhereIsMyMusic/Application/Presentation/ViewModels/ShazamSearchViewViewModel.swift
@@ -7,8 +7,22 @@
 
 import Foundation
 
-struct ShazamSearchViewViewModel {
+class ShazamSearchViewViewModel {
     let shazamSession = ShazamSession()
+    var shazamSong: Box<ShazamSong?> = Box(nil)
+    var error: Box<ShazamError?> = Box(nil)
+    var searching = Box(false)
     
-    init() {}
+    init() {
+        shazamSession.completion = {
+            switch $0 {
+            case .success(let shazamSong):
+                self.searching.value = false
+                self.shazamSong.value = shazamSong
+            case .failure(let error):
+                self.searching.value = false
+                self.error.value = error
+            }
+        }
+    }
 }

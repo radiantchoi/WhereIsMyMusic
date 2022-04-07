ff1e540e361d46c584d3272dcb64673dd195ba8b
Gordon Choi <thefirate@gmail.com>
2022-04-07 19:08:20 +0900

    Set new data calling function and test

--- WhereIsMyMusic/Networking/NetworkManager.swift
+++ WhereIsMyMusic/Networking/NetworkManager.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import RxSwift
+import Alamofire
 
 struct NetworkManager {
     static let shared = NetworkManager()
@@ -44,6 +46,40 @@ extension NetworkManager {
         }
         task.resume()
         return task
+    }
+    
+    func callTwo<T: Codable>(_ endPoint: EndPoint, for model: T.Type) -> Observable<T> {
+        let url = endPoint.baseURL.withQueries(endPoint.query ?? [:])
+        var request = URLRequest(url: url!)
+        request.httpMethod = endPoint.httpMethod.rawValue
+        
+        if endPoint.headers != nil {
+            for (key, value) in endPoint.headers! {
+                request.setValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+        return Observable<T>.create { observer in
+            let dataRequest = AF.request(request)
+                .responseData { response in
+                    switch response.result {
+                    case .success(let data):
+                        do {
+                            let model: T = try JSONDecoder().decode(T.self, from: data)
+                            observer.onNext(model)
+                        } catch (let error) {
+                            observer.onError(error)
+                        }
+                    case .failure(let error):
+                        observer.onError(error)
+                    }
+                    
+                    observer.onCompleted()
+                }
+            return Disposables.create {
+                dataRequest.cancel()
+            }
+        }
     }
 }
 

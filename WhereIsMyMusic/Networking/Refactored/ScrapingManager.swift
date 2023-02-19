6673d0a0c4ce49d6afbed191bc2c2ab7a7d64b8c
Gordon Choi <thefirate@gmail.com>
2023-02-20 02:16:01 +0900

    refactor: make new scraping manager skeleton code

---
+++ WhereIsMyMusic/Networking/Refactored/ScrapingManager.swift
@@ -0,0 +1,20 @@
+//
+//  ScrapingManager.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2023/02/20.
+//
+
+import Foundation
+
+import Alamofire
+import RxSwift
+import SwiftSoup
+
+struct ScrapingManager {
+    static let shared = ScrapingManager()
+    
+    private init() {}
+    
+    func scrap() {}
+}

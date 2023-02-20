2cee79a76590552b98792fe1ed6802041c7227f4
Gordon Choi <thefirate@gmail.com>
2023-02-21 01:57:46 +0900

    feat: add universal music searching repository

---
+++ WhereIsMyMusic/Refactored/Data/Repository/SearchMusicRepository.swift
@@ -0,0 +1,31 @@
+//
+//  SearchMusicRepository.swift
+//  WhereIsMyMusic
+//
+//  Created by Gordon Choi on 2023/02/21.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol SearchMusicRepository {
+    func callFromAPI<T>(_ endpoint: Endpoint) -> Single<T> where T: Decodable
+    func scrapFromWebsite(_ endpoint: Endpoint, css: ScrapingCSS) -> Single<[[String]]>
+}
+
+final class SearchMusicRepositoryImpl: SearchMusicRepository {
+    func callFromAPI<T>(_ endpoint: Endpoint) -> Single<T> where T: Decodable {
+        return NetworkingManager.shared
+            .call(endpoint)
+            .map {
+                let decodedResult = try JSONDecoder().decode(T.self, from: $0)
+                return decodedResult
+            }
+    }
+    
+    func scrapFromWebsite(_ endpoint: Endpoint, css: ScrapingCSS) -> Single<[[String]]> {
+        return ScrapingManager.shared
+            .scrap(endpoint, css: css)
+    }
+}

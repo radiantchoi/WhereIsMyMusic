01e3bf2bf63c4305b238cca39f5322691379cdd6
Gordon Choi <thefirate@gmail.com>
2023-05-20 03:48:34 +0900

    refactor: refactor Array slice method

--- WhereIsMyMusic/Extensions/Array+Extension.swift
+++ WhereIsMyMusic/Extensions/Array+Extension.swift
@@ -9,10 +9,8 @@ import Foundation
 
 extension Array {
     func slice(first n: Int) -> Array<Element> {
-        if self.count >= n {
-            return Array(self[0...(n-1)])
-        } else {
-            return self
-        }
+        return count >= n
+        ? Array(self[0..<n])
+        : self
     }
 }
